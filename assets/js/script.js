a8e012e20d8d11844f98d32ae1573cfa4e1c0ae0
aidyel <aidayeli@icloud.com>
2022-02-19 17:19:25 -0500

    start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
-// Build timer countdown function
+
+var startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz() {  
+  console.log('started')
+}
+
+
+
 var timerEl = document.getElementById('time')
-
-
+// Build timer countdown function
 function countdown(){
     var timeLeft = 75;
-
+    
     var timeInterval = setInterval(function () {
         if (timeLeft > 1){
             timerEl.textContent = timeLeft + 'seconds remaining';
@@ -16,38 +25,41 @@ function countdown(){
             timerEl.textContent = '';
             clearInterval(timeInterval);
         }
-
+        
     }, 1000)
 }
 // questions
 
-var question =[
-    {
-        titile: "Commonly use data types Do Not include:",
-        choices: ["Strings, booleans, alerts, numbers"],
-        solution: "2"
-    },
 
-    {
-        titile: "Commonly use data types Do Not include:",
-        choices: ["Strings", "booleans", "alerts", "numbers"],
-        solution: "2"
-    },
 
-    {
-        titile: "The condition in an if/else statement is enclosed with______. ",
-        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        solution: "2"
-    }, 
 
-    {
-        titile: "Arrays in JavaScript can be used to store______.",
-        choices: ["numbers and srtings", "other arrays", "booleans", "all of the above"],
-        solution: "3"
-    }, 
+// var question =[
+//     {
+//         titile: "Commonly use data types Do Not include:",
+//         choices: ["Strings, booleans, alerts, numbers"],
+//         solution: "2"
+//     },
 
-    {
-        titile: "String values must be enclosed within_______ when being assigned to variables",
-        choices: ["comas", "curly brackets", "quotes", "parenthesis"],
-        solution: "2"
-    },]
+//     {
+//         titile: "Commonly use data types Do Not include:",
+//         choices: ["Strings", "booleans", "alerts", "numbers"],
+//         solution: "2"
+//     },
+
+//     {
+//         titile: "The condition in an if/else statement is enclosed with______. ",
+//         choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+//         solution: "2"
+//     }, 
+
+//     {
+//         titile: "Arrays in JavaScript can be used to store______.",
+//         choices: ["numbers and srtings", "other arrays", "booleans", "all of the above"],
+//         solution: "3"
+//     }, 
+
+//     {
+//         titile: "String values must be enclosed within_______ when being assigned to variables",
+//         choices: ["comas", "curly brackets", "quotes", "parenthesis"],
+//         solution: "2"
+//     },]
